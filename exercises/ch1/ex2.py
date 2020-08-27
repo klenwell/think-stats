c7e3350d5bd88cc03fa3ed3bc83cada4d21cbf75
klenwell <klenwell@gmail.com>
2020-08-27 10:16:38 -0700

    Twists modules to work with exercise 2 script. But it is kludgey. Will switch to Cement framework.

--- exercises/ch1/ex2.py
+++ exercises/ch1/ex2.py
@@ -3,10 +3,19 @@ Think Stats 2e
 
 See Constants below.
 
+Data Source: https://www.cdc.gov/nchs/nsfg/nsfg_cycle6.htm
+
 USAGE:
     python exercises/ch1/ex2.py
 """
+# Insert root directory in PATH
 import sys
+from os.path import dirname
+ROOT_DIR = dirname(dirname(dirname(__file__)))
+sys.path.insert(0, ROOT_DIR)
+
+# Imports
+from extracts.cdc.nsfg import CdcNsfgExtract
 
 
 #
@@ -22,13 +31,15 @@ EXERCISE = 2
 #
 # python exercises/ch1/ex2.py
 def interactive(args):
+    print("Chapter {} Exercise #{}".format(CHAPTER, EXERCISE))
     print("You are interactive mode for {}".format(sys.argv[0]))
     breakpoint()
 
 
 # python exercises/ch1/ex2.py exercise
 def exercise(args):
-    pass
+    extract = CdcNsfgExtract()
+    print(extract)
 
 
 #
--- extracts/cdc/nsfg.py
+++ extracts/cdc/nsfg.py
@@ -1,3 +1,25 @@
+"""
+CDC National Survey of Family Growth
+https://www.cdc.gov/nchs/nsfg/nsfg_cycle6.htm
+
+Used with Think Stats 2e:
+http://greenteapress.com/thinkstats2/html/thinkstats2002.html
+"""
+from functools import cached_property
+import pandas
+
+
+#
+# Constants
+#
+DATA_FILE = None
+
 
 class CdcNsfgExtract:
-    pass
+    #
+    # Properties
+    #
+    @cached_property
+    def dataframe(self):
+        df = pandas.read_fwf(DATA_FILE, compression='gzip')
+        return df
