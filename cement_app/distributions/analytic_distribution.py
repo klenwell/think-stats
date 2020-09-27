ad2bf07e088769b63bd8b8eb7d93315536ed124b
klenwell <klenwell@gmail.com>
2020-09-27 12:43:06 -0700

    Stubs out new AnalyticDistribution class.

---
+++ cement_app/distributions/analytic_distribution.py
@@ -0,0 +1,25 @@
+import math
+from matplotlib import pyplot
+import numpy as np
+
+
+class AnalyticDistribution:
+    #
+    # Constructor
+    #
+    def __init__(self):
+        pass
+
+    #
+    # Properties
+    #
+
+    #
+    # Instance Methods
+    #
+    def plot(self, **options):
+        pass
+
+    #
+    # Private Methods
+    #
