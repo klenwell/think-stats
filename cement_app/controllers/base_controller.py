3396280b034d0630cf7987cf0a6e7c5f9036f390
klenwell <klenwell@gmail.com>
2020-11-02 14:09:24 -0800

    Plots chart 5.4 comparing birth weight data to a model cdf.

--- cement_app/controllers/base_controller.py
+++ cement_app/controllers/base_controller.py
@@ -13,22 +13,49 @@ class BaseController(Controller):
         from cement_app.extracts.cdc.nsfg import FamilyGrowthExtract
         from cement_app.decorators.cdf import CumulativeDistributionFunction
 
+        # Plot birthweights
         extract = FamilyGrowthExtract()
-        cdf = CumulativeDistributionFunction.from_series(extract.live_births.prglngth, 'births')
-        print(cdf)
+        cdf = CumulativeDistributionFunction.from_series(extract.live_births.totalwgt_lb, 'weight')
+        print(cdf.median)
 
-        sample = {
-            'cdf.prob(38)': cdf.prob(38),
-            'cdf.value(cdf.prob(38))': cdf.value(cdf.prob(38)),
-            'cdf.percentile_rank(38)': cdf.percentile_rank(38),
-            'cdf.percentile(cdf.percentile_rank(38))': cdf.percentile(cdf.percentile_rank(38)),
-            'cdf.median': cdf.median
-        }
-        print(sample)
+        # Plot Model
+        from scipy import stats
+        import numpy as np
 
-        chart = cdf.plot()
-        chart.show()
-        breakpoint()
+        # Birth weights sans null values
+        weights = extract.live_births.totalwgt_lb.dropna()
+
+        # Estimate parameters: trimming outliers yields a better fit
+        # Lop off high and low 1% of values
+        trimmed_margin = 0.01
+        trimmed_count = int(trimmed_margin * len(weights))
+        trimmed_weights = sorted(weights)[trimmed_count:-trimmed_count]
+
+        # Compute mean
+        weights_as_array = np.asarray(trimmed_weights)
+        mean = weights_as_array.mean()
+
+        # Compute variance
+        devs = weights_as_array - mean
+        var = np.dot(devs, devs) / len(weights_as_array)
+        sigma = np.sqrt(var)
+        print(var, sigma)
+
+        # xs, ps = thinkstats2.RenderNormalCdf(mu, sigma, low=0, high=12.5)
+        low, high, steps = 0, 12.5, 101
+        vals = np.linspace(low, high, steps)
+        probs = stats.norm.cdf(vals, mean, sigma)
+
+        model_cdf = CumulativeDistributionFunction([])
+        for weight, cum_dist in zip(vals, probs):
+            model_cdf.store[weight] = cum_dist
+
+        model_chart = model_cdf.plot()
+
+        # Plot birth chart over model chart
+        births_chart = cdf.plot()
+        births_chart.show()
+
 
     # python app.py test -f foo arg1 extra1 extra2
     @expose(
