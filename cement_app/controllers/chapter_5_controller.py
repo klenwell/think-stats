3628c801d61900dbc7ebea3d44d6a95ed151e5f0
klenwell <klenwell@gmail.com>
2020-11-08 17:06:17 -0800

    Adds section 3 graph to ch 5 controller.

--- cement_app/controllers/chapter_5_controller.py
+++ cement_app/controllers/chapter_5_controller.py
@@ -3,10 +3,12 @@ from cement import ex as expose
 
 from scipy import stats
 import numpy as np
+from matplotlib import pyplot as plt
 
 from cement_app.extracts.brisbane.births import BrisbaneBirthsExtract
 from cement_app.extracts.cdc.nsfg import FamilyGrowthExtract
 from cement_app.decorators.cdf import CumulativeDistributionFunction as CDF
+from cement_app.distributions.normal_distribution import NormalDistribution
 
 
 class Chapter5Controller(Controller):
@@ -15,44 +17,41 @@ class Chapter5Controller(Controller):
         stacked_on = 'base'
         stacked_type = 'nested'
 
+    # python app.py ch-5 s3
+    @expose(aliases=['s3'])
+    def section_5_3(self):
+        # Extract
+        extract = FamilyGrowthExtract()
+        birth_weights_series = extract.live_births.totalwgt_lb
+
+        # Weights as normal distribution
+        n = len(birth_weights_series)
+        xs = np.random.normal(0, 1, n)
+        xs.sort()
+        ys = np.array(birth_weights_series)
+        ys.sort()
+
+        options = dict(linewidth=3, alpha=0.7, label='Normal')
+        plt.plot(xs, ys, '', **options)
+
+        # As normal distribution
+        normal_distribution = NormalDistribution.model_series(birth_weights_series, -4, 4)
+        normal_chart = normal_distribution.plot_line()
+        normal_chart.show()
+
     # python app.py ch-5 s2
     @expose(aliases=['s2'])
     def section_5_2(self):
-        # Plot birthweights
+        # Extract
         extract = FamilyGrowthExtract()
-        cdf = CDF.from_series(extract.live_births.totalwgt_lb, 'weight')
-        print(cdf.median)
+        birth_weights_series = extract.live_births.totalwgt_lb
 
-        # Birth weights sans null values
-        weights = extract.live_births.totalwgt_lb.dropna()
-
-        # Estimate parameters: trimming outliers yields a better fit
-        # Lop off high and low 1% of values
-        trimmed_margin = 0.01
-        trimmed_count = int(trimmed_margin * len(weights))
-        trimmed_weights = sorted(weights)[trimmed_count:-trimmed_count]
-
-        # Compute mean
-        weights_as_array = np.asarray(trimmed_weights)
-        mean = weights_as_array.mean()
-
-        # Compute variance
-        devs = weights_as_array - mean
-        var = np.dot(devs, devs) / len(weights_as_array)
-        sigma = np.sqrt(var)
-        print(var, sigma)
-
-        # xs, ps = thinkstats2.RenderNormalCdf(mu, sigma, low=0, high=12.5)
-        low, high, steps = 0, 12.5, 101
-        vals = np.linspace(low, high, steps)
-        probs = stats.norm.cdf(vals, mean, sigma)
-
-        model_cdf = CDF([])
-        for weight, cum_dist in zip(vals, probs):
-            model_cdf.store[weight] = cum_dist
-
-        # Plot birth chart over model chart
+        # Model birthweight CDF
+        model_cdf = CDF.model_series(birth_weights_series)
         model_cdf.plot()
+
+        # Plot birthweights chart over model chart
+        cdf = CDF.from_series(birth_weights_series, 'weight')
         births_chart = cdf.plot()
         births_chart.show()
 
